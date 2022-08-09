39db9b42c7d10c17f22ae11af1c92effc8f5f373
kkng932 <88977637+kkng932@users.noreply.github.com>
2022-08-09 14:42:27 +0900

    before making navigation view

--- Raise the Poor Practice/Assets/Scripts/MoneyDirector.cs
+++ Raise the Poor Practice/Assets/Scripts/MoneyDirector.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Numerics;
 
 public class MoneyDirector : MonoBehaviour
 {
@@ -9,6 +10,14 @@ public class MoneyDirector : MonoBehaviour
     GameObject clickMoney;
     GameObject perSecond;
     GameObject propertyStatus;
+
+    float span = 1.0f;
+    float delta = 0;
+
+    BigInteger my_money= 0;
+    BigInteger click_money = 2;
+    BigInteger per_second = 2;
+    BigInteger property_status = 0;
 
     void Start()
     {
@@ -20,8 +29,22 @@ public class MoneyDirector : MonoBehaviour
     }
     void Update()
     {
-        this.myMoney.GetComponent<Text>().text = "원";
+        this.myMoney.GetComponent<Text>().text = this.my_money.ToString()+ "원";
+        this.propertyStatus.GetComponent<Text>().text = "자산현황:" + this.property_status.ToString() + "원";
 
-        this.propertyStatus.GetComponent<Text>().text = "자산현황:" + "원";
+
+        // 1초당 증가하는 돈
+        this.delta += Time.deltaTime;
+        if(this.delta>this.span)
+        {
+            this.delta = 0;
+            this.my_money += this.per_second;
+        }
+
+        // 클릭시 증가하는 돈
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
+        {
+            this.my_money += this.click_money;
+        }
     }
 }
